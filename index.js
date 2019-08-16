8fa4094cdb4b68ded8799fd270c4b2ebfe56e1cd
William Bae <wbae912@gmail.com>
2019-08-16 08:54:13 -0700

    boiler plate html created and function skeletons defined in index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+function startGame () {
+
+}
+
+/////////////
+
+function generateQuestionHtml () {
+
+}
+
+/////////////
+
+function renderQuestion () {
+
+}
+
+/////////////
+
+function questionCounter () {
+
+}
+
+/////////////
+
+function updateScore () {
+
+}
+
+/////////////
+
+function handlesFormSubmit () {
+    // preventDefault
+    // call showRight || showWrong
+}
+
+function showWrongAnswer () {
+
+}
+
+/////////////
+
+function showRightAnswer () {
+
+}
+
+/////////////
+
+function renderFinalResult () {
+
+}
+
+/////////////
+
+function restartGame () {
+
+}
