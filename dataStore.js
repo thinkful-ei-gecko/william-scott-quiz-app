f6077cdb0361190eb866be20d09c0b3e03ac6a7c
Scott Schroeder <snschroeder89@gmail.com>
2019-08-16 11:45:58 -0500

    added function for start and render question

---
+++ dataStore.js
@@ -0,0 +1,9 @@
+const dataTable = [{
+    question: 'You must get a permit to modify what in Colorado?',
+    answer1: 'The weather',
+    answer2: 'Your car',
+    answer3: 'Your house',
+    answer4: 'Your grandmother\'s will',
+    correctAnswer: 'The weather'
+    }
+]
--- index.js
+++ index.js
@@ -1,19 +1,60 @@
 'use strict';
 
-function startGame () {
+function startGame (game) {
+    $('.start-button').on('click', function(event) {
+        $(this).closest('.quiz-start').remove();
+        renderQuestion(game);
+    })
+}
 
+function game(questionNumber = 0, score = 0) {
+    return {
+        questionNumber,
+        score,
+        incrementScore() {
+            score++;
+        },
+        incrementQuestion() {
+            questionNumber++;
+        }
+    }
 }
 
 /////////////
 
-function generateQuestionHtml () {
+function generateQuestionHtml (game) {
+    let currentQuestion = game.questionNumber;
 
+    return `
+    <h2 class='question'>${dataTable[currentQuestion].question}</h2>
+    <form>
+        <div>
+            <input type="radio" name="option" id="option one">
+            <label for='option one'>${dataTable[currentQuestion].answer1}</label>
+        </div>
+        <div>
+            <input type="radio" name="option" id="option two">
+            <label for="option two">${dataTable[currentQuestion].answer2}</label>
+        </div>
+        <div>
+            <input type="radio" name="option" id="option three">
+            <label for="option three">${dataTable[currentQuestion].answer3}</label>
+        </div>
+        <div>
+            <input type="radio" name="option" id="option four">
+            <label for="option four">${dataTable[currentQuestion].answer4}</label>
+        </div>
+        <div>
+            <button type='button' class='submit-answer-button'>Submit answer</button>
+        </div>
+    </form>
+    `
 }
 
 /////////////
 
-function renderQuestion () {
-
+function renderQuestion (game) {
+    $('.quiz-form').html(generateQuestionHtml(game));
 }
 
 /////////////
@@ -30,7 +71,7 @@ function updateScore () {
 
 /////////////
 
-function handlesFormSubmit () {
+function handlesFormSubmit (game) {
     // preventDefault
     // call showRight || showWrong
 }
@@ -45,6 +86,10 @@ function showRightAnswer () {
 
 }
 
+function feedbackNextSubmit() {
+
+}
+
 /////////////
 
 function renderFinalResult () {
@@ -56,3 +101,11 @@ function renderFinalResult () {
 function restartGame () {
 
 }
+
+function main() {
+    let currentGame = game()
+    startGame (currentGame)
+
+}
+
+$(main)
